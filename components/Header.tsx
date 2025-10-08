3305dda72f305b40af70e4c20be7d906d58e94b2
emperaspera1 <alexrakh98@gmail.com>
2025-10-08 02:14:01 +0200

    Add components/Header.tsx - Generated by UltraThink AI

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Header() {
+  return (
+    <header className="bg-primary text-primary-foreground p-4">
+      <h1 className="text-2xl">Todo App</h1>
+    </header>
+  )
+}
